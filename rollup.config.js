047e646dde54fc9c3cfb79d216d8ed496327f658
MaZarrr <vitalistarkiii@gmail.com>
2021-10-09 23:50:09 +0300

    test ie

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+import babel  from '@rollup/plugin-babel';
 
 const production = !process.env.ROLLUP_WATCH;
 const legacy = !!process.env.LEGACY;
@@ -48,51 +48,186 @@ export default {
 				dev: !production
 			}
 		}),
-		legacy && babel({
-			extensions: ['.ts, .js', '.mjs', '.html', '.svelte'],
-			runtimeHelpers: true,
-			exclude: ['node_modules/@babel/**', 'node_modules/core-js/**'], // <= /!\ NOT 'node_mobules/**'
-			presets: [
-				['@babel/preset-env', {
-					// adapter to ensure IE 11 support
-					targets: '> 0.25%, not dead, IE 11',
-					useBuiltIns: 'usage'
-				}]
-			],
-			plugins: [
-				'@babel/plugin-syntax-dynamic-import',
-				'@babel/plugin-syntax-import-meta',
-				['@babel/plugin-transform-runtime', {
-					useESModules: true
-				}]
-			]
-		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
+
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
 			dedupe: ['svelte']
 		}),
 		commonjs(),
+
+		legacy && babel({
+			extensions: ['.js', '.mjs', '.html', '.svelte'],
+			babelHelpers: 'runtime',
+			exclude: ['node_modules/@babel/**'], // <= /!\ NOT 'node_mobules/**'
+			plugins: [
+				'@babel/plugin-syntax-dynamic-import',
+				'@babel/plugin-syntax-import-meta',
+				['@babel/plugin-transform-runtime', {
+					useESModules: true
+				}],
+			],
+			presets: [
+				['@babel/preset-env', {
+					// adapter to ensure IE 11 support
+					targets: '> 0.25%, not dead, IE 11',
+				}],
+			],
+		}),
+
+
 		typescript({
 			sourceMap: !production,
 			inlineSources: !production
 		}),
 
+
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
 		!production && serve(),
+
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
 		!production && livereload('public'),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
 		production && terser()
 	],
 	watch: {
 		clearScreen: false
-	},
-	onwarn: function (warning, superOnWarn) {
-		/*
-         * skip certain warnings
-         * https://github.com/openlayers/openlayers/issues/10245
-         */
-		if (warning.code === 'THIS_IS_UNDEFINED') {
-			return;
-		}
-		superOnWarn(warning);
 	}
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// import svelte from 'rollup-plugin-svelte';
+// import commonjs from '@rollup/plugin-commonjs';
+// import livereload from 'rollup-plugin-livereload';
+// import { terser } from 'rollup-plugin-terser';
+// import sveltePreprocess from 'svelte-preprocess';
+// import typescript from '@rollup/plugin-typescript';
+// import css from 'rollup-plugin-css-only';
+// import resolve from '@rollup/plugin-node-resolve';
+// import babel from '@rollup/plugin-babel';
+//
+// const production = !process.env.ROLLUP_WATCH;
+// const legacy = !!process.env.LEGACY;
+//
+// function serve() {
+// 	let server;
+//
+// 	function toExit() {
+// 		if (server) server.kill(0);
+// 	}
+//
+// 	return {
+// 		writeBundle() {
+// 			if (server) return;
+// 			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+// 				stdio: ['ignore', 'inherit', 'inherit'],
+// 				shell: true
+// 			});
+//
+// 			process.on('SIGTERM', toExit);
+// 			process.on('exit', toExit);
+// 		}
+// 	};
+// }
+//
+// export default {
+// 	input: 'src/main.ts',
+// 	output: {
+// 		sourcemap: true,
+// 		format: 'iife',
+// 		name: 'app',
+// 		file: 'public/build/bundle.js'
+// 	},
+// 	plugins: [
+// 		svelte({
+// 			preprocess: sveltePreprocess({ sourceMap: !production }),
+// 			compilerOptions: {
+// 				// enable run-time checks when not in production
+// 				dev: !production
+// 			}
+// 		}),
+// 		legacy && babel({
+// 			extensions: ['.ts, .js', '.mjs', '.html', '.svelte'],
+// 			runtimeHelpers: true,
+// 			exclude: ['node_modules/@babel/**', 'node_modules/core-js/**'], // <= /!\ NOT 'node_mobules/**'
+// 			presets: [
+// 				['@babel/preset-env', {
+// 					// adapter to ensure IE 11 support
+// 					targets: '> 0.25%, not dead, IE 11',
+// 					useBuiltIns: 'usage'
+// 				}]
+// 			],
+// 			plugins: [
+// 				'@babel/plugin-syntax-dynamic-import',
+// 				'@babel/plugin-syntax-import-meta',
+// 				['@babel/plugin-transform-runtime', {
+// 					useESModules: true
+// 				}]
+// 			]
+// 		}),
+// 		// we'll extract any component CSS out into
+// 		// a separate file - better for performance
+// 		css({ output: 'bundle.css' }),
+//
+// 		// If you have external dependencies installed from
+// 		// npm, you'll most likely need these plugins. In
+// 		// some cases you'll need additional configuration -
+// 		// consult the documentation for details:
+// 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+// 		resolve({
+// 			browser: true,
+// 			dedupe: ['svelte']
+// 		}),
+// 		commonjs(),
+// 		typescript({
+// 			sourceMap: !production,
+// 			inlineSources: !production
+// 		}),
+//
+// 		// In dev mode, call `npm run start` once
+// 		// the bundle has been generated
+// 		!production && serve(),
+//
+// 		// Watch the `public` directory and refresh the
+// 		// browser on changes when not in production
+// 		!production && livereload('public'),
+//
+// 		// If we're building for production (npm run build
+// 		// instead of npm run dev), minify
+// 		production && terser()
+// 	],
+// 	watch: {
+// 		clearScreen: false
+// 	},
+// 	onwarn: function (warning, superOnWarn) {
+// 		/*
+//          * skip certain warnings
+//          * https://github.com/openlayers/openlayers/issues/10245
+//          */
+// 		if (warning.code === 'THIS_IS_UNDEFINED') {
+// 			return;
+// 		}
+// 		superOnWarn(warning);
+// 	}
+// };
